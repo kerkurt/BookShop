b54a53a14a8b43b70bc5dad45358ea7c9083a8ff
kerkurt <106493519+kerkurt@users.noreply.github.com>
2022-06-06 01:02:19 +0300

    Category view created, bootstrap 5 integrations applied and category creating functionality added

---
+++ BookShopWeb/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using BookShopWeb.Data;
+using BookShopWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShopWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext m_Db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            m_Db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categoryList = m_Db.Categories;
+            return View(categoryList);
+        }
+
+        //Get
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category objCategory)
+        {
+            m_Db.Categories.Add(objCategory);
+            m_Db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
