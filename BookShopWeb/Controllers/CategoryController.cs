4e2128df93d9b722f0f7289fc094e0fac0f41106
kerkurt <106493519+kerkurt@users.noreply.github.com>
2022-06-06 23:08:01 +0300

    Added the feature for editing the existing category.

--- BookShopWeb/Controllers/CategoryController.cs
+++ BookShopWeb/Controllers/CategoryController.cs
@@ -43,5 +43,45 @@ namespace BookShopWeb.Controllers
             }
             return View(objCategory);
         }
+
+        //Get
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = m_Db.Categories.Find(id); //Id' nin primary key olduğunu bildiğimiz durumlarda
+            //var categoryFromDbFirst = m_Db.Categories.FirstOrDefault(x => x.Id == id); //İlk bulduğu id' yi getirir, birden fazla varsa exception üretmez.
+            //var categoryFromDbSingle = m_Db.Categories.SingleOrDefault(x => x.Id == id); //Birden fazla id varsa exception fırlatır.
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category objCategory)
+        {
+            //if (objCategory.Name == objCategory.DisplayOrder.ToString()) 
+            //{
+            //    ModelState.AddModelError("CustomError", "The DisplayOrder can't exactly match the Name!");//view içerisinde asp-validation-summary tag helper eklediğimiz için kullanabiliyoruz.
+            //    //ModelState.AddModelError("Name", "The DisplayOrder can't exactly match the Name!"); //Yukarıda CustomError vermesiyle birlikte Name field' ının altında da custom summary görünmesini istersek.
+            //}
+            if (ModelState.IsValid)
+            {
+                m_Db.Categories.Update(objCategory);
+                m_Db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(objCategory);
+        }
+
+
     }
 }
